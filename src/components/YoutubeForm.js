b1dab3adb81c449e3dc1de210f7fee7ab3a65416
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-03-16 14:31:39 -0500

    [ADD] managing form state

---
+++ src/components/YoutubeForm.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useFormik } from "formik";
+
+const initialValues = {
+  name: "",
+  email: "",
+  channel: "",
+};
+
+export const YoutubeForm = () => {
+  const formik = useFormik({ initialValues });
+
+  console.log('Form values:', formik.values)
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+
+        <label htmlFor="channel">Channel</label>
+        <input
+          type="text"
+          id="channel"
+          channel="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
