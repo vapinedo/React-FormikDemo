2f6f55babebd70c8db98375c2b954a9b013f6822
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-03-22 13:05:38 -0500

    [ADD] input reusable

--- src/components/FormikContainter.js
+++ src/components/FormikContainter.js
@@ -1,24 +1,29 @@
-import React from 'react'
-import { Formik, Form } from 'formik'
-import * as Yup from 'yup'
+import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { FormikControl } from "./FormikControl";
 
 export const FormikContainter = () => {
+  const initialValues = {
+    email: "",
+  };
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Required"),
+  });
+  const onSubmit = (values) => console.log("Form data", values);
 
-    const initialValues = {};
-    const validationSchema = Yup.object({});
-    const onSubmit = values => console.log('Form data', values);
-
-    return (
-        <Formik 
-            initialValues={initialValues} 
-            validationSchema={validationSchema}
-            onSubmit={onSubmit}
-        >
-            {formik => 
-                <Form>
-                    <button type='submit'>Submit</button>
-                </Form>
-            }
-        </Formik>
-    )
-}
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {(formik) => (
+        <Form>
+          <FormikControl control="input" type="email" label="Email" name="email" />
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
---
+++ src/components/FormikControl.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Input } from './Input';
+
+export const FormikControl = (props) => {
+    const { control, ...rest } = props;
+    
+    switch(control) {
+        case 'input':
+            return <Input {...rest} />
+        case 'textarea':
+        case 'select':
+        case 'radio':
+        case 'checkbox':
+        case 'date':
+        default: return null;
+    }
+}
